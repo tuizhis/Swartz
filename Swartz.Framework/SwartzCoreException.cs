001e20c6a05d83a854d6b6893e0bda383c253375
xqlun <xql@feiniubus.com>
2016-05-05 11:56:01 +0800

    提交基础接口及异常类

---
+++ Swartz.Framework/SwartzCoreException.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Runtime.Serialization;
+
+namespace Swartz
+{
+    [Serializable]
+    public class SwartzCoreException : Exception
+    {
+        public SwartzCoreException(string message) : base(message)
+        {
+        }
+
+        public SwartzCoreException(string message, Exception innerException) : base(message, innerException)
+        {
+        }
+
+        protected SwartzCoreException(SerializationInfo info, StreamingContext context) : base(info, context)
+        {
+        }
+    }
+}
---
+++ Swartz.Framework/SwartzException.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Runtime.Serialization;
+
+namespace Swartz
+{
+    [Serializable]
+    public class SwartzException : ApplicationException
+    {
+        public SwartzException(string message) : base(message)
+        {
+        }
+
+        public SwartzException(string message, Exception innerException) : base(message, innerException)
+        {
+        }
+
+        protected SwartzException(SerializationInfo info, StreamingContext context) : base(info, context)
+        {
+        }
+    }
+}
