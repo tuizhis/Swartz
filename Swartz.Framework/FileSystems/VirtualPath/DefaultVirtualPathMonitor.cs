d284262e15420fc09c0f300e6847f90972e12778
xqlun <xql@feiniubus.com>
2016-05-05 16:00:14 +0800

    完成文件系统的编写

--- Swartz.Framework/FileSystems/VirtualPath/DefaultVirtualPathMonitor.cs
+++ Swartz.Framework/FileSystems/VirtualPath/DefaultVirtualPathMonitor.cs
@@ -1,25 +1,122 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.Web;
 using System.Web.Caching;
+using System.Web.Hosting;
 using Swartz.Caching;
+using Swartz.Logging;
+using Swartz.Services;
 
 namespace Swartz.FileSystems.VirtualPath
 {
     public class DefaultVirtualPathMonitor : IVirtualPathMonitor
     {
+        private readonly IClock _clock;
+        private readonly string _prefix = Guid.NewGuid().ToString("n");
+        private readonly Thunk _thunk;
+        private readonly IDictionary<string, Weak<Token>> _tokens = new Dictionary<string, Weak<Token>>();
+
+        public DefaultVirtualPathMonitor(IClock clock)
+        {
+            _clock = clock;
+            _thunk = new Thunk(this);
+            Logger = NullLogger.Instance;
+        }
+
+        public ILogger Logger { get; set; }
+
         public IVolatileToken WhenPathChanges(string virtualPath)
         {
-            throw new NotImplementedException();
+            var token = BindToken(virtualPath);
+            try
+            {
+                BindSignal(virtualPath);
+            }
+            catch (HttpException e)
+            {
+                // This exception happens if trying to monitor a directory or file
+                // inside a directory which doesn't exist
+                Logger.Information(e, "Error monitoring file changes on virtual path '{0}'", virtualPath);
+
+                //TODO: Return a token monitoring first existing parent directory.
+            }
+
+            return token;
         }
 
         public void Signal(string key, object value, CacheItemRemovedReason reason)
         {
             var virtualPath = Convert.ToString(value);
-            
-            
+            Logger.Debug("Virtual path changed ({1}) '{0}'", virtualPath, reason.ToString());
+
+            var token = DetachToken(virtualPath);
+            if (token != null)
+            {
+                token.IsCurrent = false;
+            }
+        }
+
+        private Token BindToken(string virtualPath)
+        {
+            lock (_tokens)
+            {
+                Weak<Token> weak;
+                if (!_tokens.TryGetValue(virtualPath, out weak))
+                {
+                    weak = new Weak<Token>(new Token(virtualPath));
+                    _tokens[virtualPath] = weak;
+                }
+
+                var token = weak.Target;
+                if (token == null)
+                {
+                    token = new Token(virtualPath);
+                    weak.Target = token;
+                }
+
+                return token;
+            }
+        }
+
+        private Token DetachToken(string virtualPath)
+        {
+            lock (_tokens)
+            {
+                Weak<Token> weak;
+                if (!_tokens.TryGetValue(virtualPath, out weak))
+                {
+                    return null;
+                }
+
+                var token = weak.Target;
+                weak.Target = null;
+                return token;
+            }
+        }
+
+        private void BindSignal(string virtualPath)
+        {
+            BindSignal(virtualPath, _thunk.Signal);
+        }
+
+        private void BindSignal(string virtualPath, CacheItemRemovedCallback callback)
+        {
+            var key = _prefix + virtualPath;
+
+            //PERF: Don't add in the cache if already present. Creating a "CacheDependency"
+            //      object (below) is actually quite expensive.
+            if (HostingEnvironment.Cache.Get(key) != null)
+            {
+                return;
+            }
+
+            var cacheDependency = HostingEnvironment.VirtualPathProvider.GetCacheDependency(virtualPath,
+                new[] {virtualPath}, _clock.UtcNow);
+
+            Logger.Debug("Monitoring virtual path \"{0}\"", virtualPath);
+
+            HostingEnvironment.Cache.Add(key, virtualPath, cacheDependency, Cache.NoAbsoluteExpiration,
+                Cache.NoSlidingExpiration, CacheItemPriority.NotRemovable, callback);
         }
 
         public class Token : IVolatileToken
@@ -30,8 +127,9 @@ namespace Swartz.FileSystems.VirtualPath
                 VirtualPath = virtualPath;
             }
 
-            public bool IsCurrent { get; set; }
             public string VirtualPath { get; }
+
+            public bool IsCurrent { get; set; }
 
             public override string ToString()
             {
@@ -39,7 +137,7 @@ namespace Swartz.FileSystems.VirtualPath
             }
         }
 
-        class Thunk
+        private class Thunk
         {
             private readonly Weak<DefaultVirtualPathMonitor> _weak;
 
