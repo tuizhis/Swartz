94847812b4c1812d0fbae0d0af07153f2ae1a1af
xqlun <xql@feiniubus.com>
2016-05-06 19:56:35 +0800

    提交Redis缓存管理模块代码

---
+++ Swartz.Redis/Caching/RedisCacheStorageProvider.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using StackExchange.Redis;
+using Swartz.Caching;
+using Swartz.Environment.Configuration;
+using Swartz.Redis.Configuration;
+using Swartz.Redis.Extensions;
+using Swartz.Services;
+
+namespace Swartz.Redis.Caching
+{
+    public class RedisCacheStorageProvider : Component, ICacheStorageProvider
+    {
+        public const string ConnectionStringKey = "Swartz.Redis.Cache";
+        private readonly ConnectionMultiplexer _connectionMultiplexer;
+        private readonly IJsonConverter _jsonConverter;
+
+        private readonly ShellSettings _shellSettings;
+
+        public RedisCacheStorageProvider(ShellSettings shellSettings, IRedisConnectionProvider redisConnectionProvider,
+            IJsonConverter jsonConverter)
+        {
+            _shellSettings = shellSettings;
+            var redisConnectionProvider1 = redisConnectionProvider;
+            var connectionString = redisConnectionProvider1.GetConnectionString(ConnectionStringKey);
+            _connectionMultiplexer = redisConnectionProvider1.GetConnection(connectionString);
+            _jsonConverter = jsonConverter;
+        }
+
+        public IDatabase Database => _connectionMultiplexer.GetDatabase();
+
+        public object Get<T>(string key)
+        {
+            var json = Database.StringGet(GetLocalizedKey(key));
+            if (string.IsNullOrEmpty(json))
+            {
+                return default(T);
+            }
+
+            return _jsonConverter.Deserialize<T>(json);
+        }
+
+        public void Put<T>(string key, T value)
+        {
+            var json = _jsonConverter.Serialize(value);
+            Database.StringSet(GetLocalizedKey(key), json);
+        }
+
+        public void Put<T>(string key, T value, TimeSpan validFor)
+        {
+            var json = _jsonConverter.Serialize(value);
+            Database.StringSet(GetLocalizedKey(key), json, validFor);
+        }
+
+        public void Remove(string key)
+        {
+            Database.KeyDelete(GetLocalizedKey(key));
+        }
+
+        public void Clear()
+        {
+            Database.KeyDeleteWithPrefix(GetLocalizedKey("*"));
+        }
+
+        private string GetLocalizedKey(string key)
+        {
+            return _shellSettings.Name + ":Cache:" + key;
+        }
+    }
+}
---
+++ Swartz.Redis/Configuration/RedisConnectionProvider.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Concurrent;
+using System.Configuration;
+using StackExchange.Redis;
+using Swartz.Environment.Configuration;
+using Swartz.Logging;
+
+namespace Swartz.Redis.Configuration
+{
+    public class RedisConnectionProvider : IRedisConnectionProvider
+    {
+        private static readonly ConcurrentDictionary<string, Lazy<ConnectionMultiplexer>> ConnectionMultiplexers =
+            new ConcurrentDictionary<string, Lazy<ConnectionMultiplexer>>();
+
+        private readonly ShellSettings _shellSettings;
+
+        public RedisConnectionProvider(ShellSettings settings)
+        {
+            _shellSettings = settings;
+            Logger = NullLogger.Instance;
+        }
+
+        public ILogger Logger { get; set; }
+
+        public ConnectionMultiplexer GetConnection(string connectionString)
+        {
+            if (string.IsNullOrWhiteSpace(connectionString))
+            {
+                throw new ArgumentNullException(nameof(connectionString));
+            }
+
+            // when using ConcurrentDictionary, multiple threads can create the value
+            // at the same time, so we need to pass a Lazy so that it's only 
+            // the object which is added that will create a ConnectionMultiplexer,
+            // even when a delegate is passed
+
+            return
+                ConnectionMultiplexers.GetOrAdd(connectionString,
+                    new Lazy<ConnectionMultiplexer>(() => ConnectionMultiplexer.Connect(connectionString))).Value;
+        }
+
+        public string GetConnectionString(string service)
+        {
+            var tenantSettingsKey = _shellSettings.Name + ":" + service;
+            var defaultSettingsKey = service;
+
+            var connectionStringSettings = ConfigurationManager.ConnectionStrings[tenantSettingsKey] ??
+                                           ConfigurationManager.ConnectionStrings[defaultSettingsKey];
+
+            if (connectionStringSettings == null)
+            {
+                throw new ConfigurationErrorsException("A connection string is expected for " + service);
+            }
+
+            return connectionStringSettings.ConnectionString;
+        }
+    }
+}
