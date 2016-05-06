2527b09675dc97851f60be40d2be7857bf549d6c
xqlun <xql@feiniubus.com>
2016-05-06 19:34:55 +0800

    提交缓存管理模块

---
+++ Swartz.Caching/ICacheService.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Swartz.Caching
+{
+    public interface ICacheService : IDependency
+    {
+        object GetObject<T>(string key);
+
+        void Put<T>(string key, T value);
+        void Put<T>(string key, T value, TimeSpan validFor);
+
+        void Remove(string key);
+        void Clear();
+    }
+}
