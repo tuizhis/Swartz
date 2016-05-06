9e7387a43a875eb703ad111086a1ed156d946569
xqlun <xql@feiniubus.com>
2016-05-06 19:04:01 +0800

    提交邮件发送模块代码

---
+++ Swartz.Email/DefaultEmailMessageChannelSelector.cs
@@ -0,0 +1,29 @@
+﻿using Swartz.Messaging;
+
+namespace Swartz.Email
+{
+    public class DefaultEmailMessageChannelSelector : Component, IMessageChannelSelector
+    {
+        private readonly IWorkContextAccessor _workContextAccessor;
+
+        public DefaultEmailMessageChannelSelector(IWorkContextAccessor workContextAccessor)
+        {
+            _workContextAccessor = workContextAccessor;
+        }
+
+        public MessageChannelSelectorResult GetChannel(MessagingType messagingType, object payload)
+        {
+            if (messagingType == MessagingType.Email)
+            {
+                var workContext = _workContextAccessor.GetContext();
+                return new MessageChannelSelectorResult
+                {
+                    Priority = 50,
+                    MessageChannel = () => workContext.Resolve<ISmtpChannel>()
+                };
+            }
+
+            return null;
+        }
+    }
+}
